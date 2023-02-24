0e653a2f0a7724995c7f47c7efc23014c1461bd4
Gabri6 <starling1220@gmail.com>
2023-02-24 05:29:17 -0500

    added webcam capture and image discrading at the start

--- PPG.cpp
+++ PPG.cpp
@@ -5,6 +5,46 @@
 
 int main() 
 {
-  std::cout << "PPG algorithm"<<std::endl;
-  return 0;
+	float FPS = 30.0;
+	
+	bool isDiscardData = true;
+	int countDiscard = 0;
+	
+	std::cout << "PPG algorithm"<<std::endl;
+	
+	cv::VideoCapture cap;
+	cap.open(0);
+	if (!cap.isOpened())
+	{
+		std::cerr << "[ERROR] Unable to open camera!" << std::endl;
+		return -2;
+	}
+	if (isDiscardData)
+	{
+		countDiscard++;
+		if (countDiscard == DISCARD_DURATION*FPS)
+		isDiscardData = false;
+	}
+	else
+	{
+		
+		while (true)
+		{
+			// create a matrix to store the image from the cam
+			cv::Mat frame;
+			// wait for a new frame from camera and store it into 'frame'
+			cap.read(frame);
+			// check if we succeeded
+			if (frame.empty())
+			{
+				std::cerr << "[ERROR] blank frame grabbed" << std::endl;
+				break;
+			}
+			cv::imshow("Color", frame);
+			if (cv::waitKey(1000.0/FPS) >= 0)
+			break;
+		}
+	}
+	
+	return 0;
 } 
