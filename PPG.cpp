67f93cf01e5054d1632fb054ebf4988b0280d074
Gabri6 <starling1220@gmail.com>
2023-02-24 05:56:02 -0500

    webcam acquiqition, face and forehead detection added

--- PPG.cpp
+++ PPG.cpp
@@ -7,12 +7,19 @@ int main()
 {
 	float FPS = 30.0;
 	float DISCARD_DURATION = 2.0;
-	
+	cv::Mat frame_gray;
 	bool isDiscardData = true;
 	int countDiscard = 0;
 	
 	std::cout << "PPG algorithm"<<std::endl;
 	
+	cv::CascadeClassifier faceDetector;
+	if( !faceDetector.load("./haarcascade_frontalface_alt.xml"))
+	{
+		std::cerr << "[ERROR] Unable to load face cascade" << std::endl;
+		return -1;
+	};
+
 	cv::VideoCapture cap;
 	cap.open(0);
 	if (!cap.isOpened())
@@ -41,9 +48,21 @@ int main()
 				std::cerr << "[ERROR] blank frame grabbed" << std::endl;
 				break;
 			}
+			
+			std::vector<cv::Rect> faceRectangles;
+			faceDetector.detectMultiScale(frame_gray, faceRectangles, 1.1, 3, 0,
+			cv::Size(20, 20));
+			
+			cv::rectangle(frame, faceRectangles[0], cv::Scalar(0, 0, 255), 1, 1, 0);
+			
+			cv::Rect foreheadROI;
+			foreheadROI = faceRectangles[0];
+			foreheadROI.height *= 0.3;
+			
 			cv::imshow("Color", frame);
 			if (cv::waitKey(1000.0/FPS) >= 0)
 			break;
+			
 		}
 	}
 	
